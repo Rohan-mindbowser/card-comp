c6a99b7189e9260dfebdfc6b3ddc72e0921be714
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-09-18 14:13:47 +0530

    responsive card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'card-comp';
+
+  data: any[] = [
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+    {
+      projectName: 'Metrics Worklog Test',
+      description: 'This project is for Worklog testing DO NOT DELETE .',
+      technologies: ['java', 'angular', 'aws', 'react', 'Node js'],
+      team: ['Rohan kadam', 'Shubham pawar', 'Amaresh joshi'],
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
